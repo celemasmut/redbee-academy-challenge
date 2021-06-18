620e50623e91ea7cf9855995f6cb6ac89bc0c9db
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-06-17 21:49:09 -0300

    compare array numbers

--- src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
+++ src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
@@ -13,9 +13,29 @@ public class CompareArrayNumbers {
    * @return The result of a + b
    */
   public static List<Integer> max(List<Integer> a, List<Integer> b) {
-    //TODO: Implement me
-    return null;
+    List<Integer> newList = new ArrayList<>();
+    int max;
+    if(a.size() == b.size()){
+      for(int i =0; i < a.size();i++){
+        max = Math.max(a.get(i),b.get(i));
+        newList.add(max);
+      }
+    }else {
+      List<Integer> shorter = new ArrayList<>();
+      shorter = a.size() < b.size() ? a : b;
+      List<Integer> greater = new ArrayList<>();
+      greater = a.size() > b.size() ? a : b;
+      for(int i =0; i < shorter.size();i++){
+        max = Math.max(a.get(i),b.get(i));
+        newList.add(max);
+      }
+      for(int i = shorter.size()-1; i < greater.size(); i++){
+        newList.add(greater.get(i));
+      }
+    }
+    return newList;
   }
+
 
 
 
